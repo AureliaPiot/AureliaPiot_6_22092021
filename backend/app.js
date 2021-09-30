b418edc7904f78fb746eb128773e52d0a2dc5322
AureliaPiot <aurelia.piot@hotmail.com>
2021-09-30 12:18:31 +0200

    installation express + fichier server.js fait

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app =express();
+
+// ajouter la connection a la DB avec moongose
+// faire le middleware Header
+
+
+
+module.exports =app;
+// exportation de l'app
