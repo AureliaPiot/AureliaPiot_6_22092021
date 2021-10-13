9d9f21bbf98ed045344fb040c86d0a2c18e6f805
AureliaPiot <aurelia.piot@hotmail.com>
2021-10-13 19:57:47 +0200

    cryptage de l'email

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app =express();
 // utilisation de express pour créer une application
 
+const crypto = require('crypto-js');
+// var AES = require("crypto-js/aes");
+
+
 const mongoose =require('mongoose');
 mongoose.connect(process.env.MONGODB_CONNECT,{ 
     useNewUrlParser: true,
@@ -16,6 +20,7 @@ const userRoutes = require('./routes/user');
 // on appel les routes des user( pour recupe la fin de l'url + le controller a efectuer)
 const sauceRoutes = require('./routes/sauce');
 const path = require('path');
+const { stringify } = require('querystring');
 // possibiliter d'enregistrer des images
 
 
@@ -25,7 +30,7 @@ const path = require('path');
 app.use(express.json());
 // pour lire le corp de la requete
 
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 // pour lire l'url de la requete
 
 
@@ -39,6 +44,7 @@ app.use((req,res,next)=>{
     //autorisation d'utiliser certain header(en-tete)
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
     //autorisation d'utiliser certaines methodes (get,post..ect)
+    res.set("Content-Security-Policy", "default-src 'self'");
     next();
 });
 
@@ -52,7 +58,22 @@ app.use('/api/sauces/',sauceRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 // possibiliter d'enregistrer des images
 
+// --------------------------------------------------
+// const test = crypto.AES.encrypt('key','elements').toString();
+// console.log("1test " +test);
+// const untest = crypto.AES.decrypt(test,'elements').toString(crypto.enc.Utf8);
+// console.log("2test " +untest);
 
+// const email= crypto.enc.Base64.stringify(crypto.SHA1(req.body.email, process.env.SELMAIL));
+// CryptoJS.AES.encrypt(msg, key, { mode: CryptoJS.mode.ECB });
+const test = crypto.AES.encrypt('key',crypto.enc.Utf8.parse('elements'), {iv: crypto.enc.Base64.parse(process.env.CRYPTOPARSE) }).toString();
+console.log("1test " +test);
+
+const untest = crypto.AES.encrypt('key',crypto.enc.Utf8.parse('elements'),{ iv: crypto.enc.Base64.parse(process.env.CRYPTOPARSE) }).toString();
+console.log("2test " +untest);
+
+console.log(test == untest)
+// --------------------------------------------------
 
 module.exports =app;
 // exportation de l'app
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,27 +7,37 @@ const bcrypt =require('bcrypt');
 // installation et importation de jwt pour crypter le token 
 const jwt = require('jsonwebtoken');
 
+// installation et importation de crypto pour crypter l'email 
+const crypto = require('crypto-js');
 
 // inscription
 exports.signup = (req, res, next)=>{
     // faire la regex
-    const regexEmail =/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(req.body.password);
-    console.log(regexEmail);
+    const regexPassword =/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(req.body.password);
+    console.log(regexPassword);
     
-    if(req.body.password && regexEmail){
-        bcrypt.hash(req.body.password, 10)
+    if(req.body.password && regexPassword){
+
+        const email=crypto.AES.encrypt(req.body.email,crypto.enc.Utf8.parse(process.env.SELMAIL),{ iv: crypto.enc.Base64.parse(process.env.CRYPTOPARSE) }).toString();
+        console.log("email 1 "+email)
+
+        
+        
+        let date= Date.now()
+        bcrypt.hash(date + req.body.password + process.env.SEL,10)
         .then(hash =>{
             const user = new User({ //utilisation du model creer et importer
-                email:req.body.email,
+                email:email,
                 password : hash,
-                date : Date.now()
+                date : date
             });
             user.save() //enregistrement de cette objet creer avec le model
                 .then(()=> res.status(201).json({message : 'utilisateur créé'}))
                 .catch(error => res.status(403).json({ error:"creation utilisateur impossible" }));
         })
         .catch(error => res.status(500).json({ error }))
-    }
+        }
+
     else{
         res.status(400).json({ message : "mot de passe invalide , il doit comporter au moins huit caractères, une lettre, un chiffre et un caractère spécial" })
     }
@@ -35,30 +45,35 @@ exports.signup = (req, res, next)=>{
 };
 
 
+
 // connexion
 exports.login = (req, res, next)=>{
-    User.findOne({email: req.body.email})
+    const email=crypto.AES.encrypt(req.body.email,crypto.enc.Utf8.parse(process.env.SELMAIL),{ iv: crypto.enc.Base64.parse(process.env.CRYPTOPARSE) }).toString();
+
+
+    User.findOne({email:email})
     .then(User => {
-        console.log('user:'+User);
+        console.log('user:'+ User);
         if(!User){
             return res.status(401).json({error:'utilisateur inexistant'})
         }
-        bcrypt.compare(req.body.password, User.password)
-    .then(valid => {
-        console.log('user valide:'+ valid);
-        if(!valid){
-            return res.status(401).json({error:'mot de passe incorrect'})
-            }
-        res.status(200).json({
-            userId: User._id,
-            token: jwt.sign(
-                { userId:User._id },
-                process.env.TOKEN,
-                { expiresIn: '24h' }
-            )
-        });
-    })
-    .catch(error => res.status(500).json({ error }))
+
+        bcrypt.compare(User.date + req.body.password + process.env.SEL ,User.password)
+        .then(valid => {
+            console.log('user valide:'+ valid);
+            if(!valid){
+                return res.status(401).json({error:'mot de passe incorrect'})
+                }
+            res.status(200).json({
+                userId: User._id,
+                token: jwt.sign(
+                    { userId:User._id },
+                    process.env.TOKEN,
+                    { expiresIn: '24h' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }))
     })
     .catch(error => res.status(500).json({ error }));
 }
