c45780154d677682e84ae732660831adaaa408d6
AureliaPiot <aurelia.piot@hotmail.com>
2022-03-08 10:41:29 +0100

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,3 +1,32 @@
+// const multer = require('multer');
+// // on appele multer qu''on a installer
+
+// // on creer une bibliotheque d"extension
+// const MIME_TYPES = {
+//   'image/jpg': 'jpg',
+//   'image/jpeg': 'jpg',
+//   'image/png': 'png'
+// };
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   filename: (req, file, callback) => {
+//     const regexInput= /([ <>{}'"=:.,;!\?\\\/\]\[])+/g ;
+
+//     const name = JSON.parse(req.body.sauce).name.replaceAll(regexInput,"-");
+
+
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null,name +"_"+ Date.now() + '.' + extension);
+//   }
+// });
+
+// module.exports = multer({storage: storage}).single('image');
+// // puis on exporte le middleware
+
+
 const multer = require('multer');
 // on appele multer qu''on a installer
 
@@ -9,7 +38,7 @@ const MIME_TYPES = {
 };
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
+  destination: (req, callback) => {
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
